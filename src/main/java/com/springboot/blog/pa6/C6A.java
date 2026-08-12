0de17948e5003d20726596e3ea2bd332a73e48c4
Ying Wang <yingwang@yingqwangVTRWM.local>
2026-08-13 01:22:26 +0800

    test(ds): cross-package import cycle (expect DS alert)

---
+++ src/main/java/com/springboot/blog/pa6/C6A.java
@@ -0,0 +1,3 @@
+package com.springboot.blog.pa6;
+import com.springboot.blog.pb6.C6B;
+public class C6A { }
---
+++ src/main/java/com/springboot/blog/pb6/C6B.java
@@ -0,0 +1,3 @@
+package com.springboot.blog.pb6;
+import com.springboot.blog.pa6.C6A;
+public class C6B { }
