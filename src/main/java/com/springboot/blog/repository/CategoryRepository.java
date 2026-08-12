5c46888833c6d078bbbe06f91932b128ae14da5b
Ying Wang <yingwang@yingqwangVTRWM.local>
2026-08-13 01:22:47 +0800

    test(bc): repository imports controller (expect BC violation)

--- src/main/java/com/springboot/blog/repository/CategoryRepository.java
+++ src/main/java/com/springboot/blog/repository/CategoryRepository.java
@@ -1,4 +1,5 @@
 package com.springboot.blog.repository;
+import com.springboot.blog.controller.PostController;
 
 import com.springboot.blog.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
